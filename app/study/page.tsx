cdf730173f09c55dba578e7f4d0cb906f640483c
panworld50-sudo <panworld50@gmail.com>
2026-03-01 02:22:58 +0530

    Create StudyPage component for study materials

---
+++ app/study/page.tsx
@@ -0,0 +1,8 @@
+export default function StudyPage() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-yellow-400">Study Section</h1>
+      <p>10th, 12th, Govt Jobs materials...</p>
+    </div>
+  );
+}
