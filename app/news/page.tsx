f4ec7d34c2be57e18c6636e386435df39b7337ee
panworld50-sudo <panworld50@gmail.com>
2026-03-01 02:22:17 +0530

    Create NewsPage component for news section

---
+++ app/news/page.tsx
@@ -0,0 +1,9 @@
+export default function NewsPage() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-yellow-400">News Section</h1>
+      <p>Latest news here...</p>
+      {/* Infinite scroll or list add kar sakte ho */}
+    </div>
+  );
+}
